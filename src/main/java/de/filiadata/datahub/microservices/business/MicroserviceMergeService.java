21152e38d4245ea3fb3b423c4e3e5f1d115ea495
Arnold Franke <arnold.franke@dm.de>
2017-11-24 14:02:16 +0100

    SELAVI-57 simplify code to merge lists of microservices

--- src/main/java/de/filiadata/datahub/microservices/business/MicroserviceMergeService.java
+++ src/main/java/de/filiadata/datahub/microservices/business/MicroserviceMergeService.java
@@ -10,80 +10,95 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
 public class MicroserviceMergeService {
 
-    public Map<String, MicroserviceDto> mergeCompleteMicroservices(final Map<String, MicroserviceDto> microservicesFromRegistry, final Map<String, MicroserviceDto> microservicesFromPersistence){
-        final Map<String, MicroserviceDto> result = new HashMap<>();
-        for (final MicroserviceDto microserviceFromRegistry : microservicesFromRegistry.values()){
-            final MicroserviceDto dto = mergeMicroservice(new MicroserviceDto(), microserviceFromRegistry);
-            result.put(dto.getId(), mergeMicroservice(dto, microservicesFromPersistence.get(microserviceFromRegistry.getId())));
-        }
-        for (final MicroserviceDto microserviceFromPersistence : microservicesFromPersistence.values()){
-            if (microservicesFromRegistry.get(microserviceFromPersistence.getId()) == null){
-                result.put(microserviceFromPersistence.getId(), mergeMicroservice(new MicroserviceDto(), microservicesFromPersistence.get(microserviceFromPersistence.getId())));
-            }
-        }
+    public Map<String, MicroserviceDto> mergeCompleteMicroservices(final Map<String, MicroserviceDto> microservicesFromRegistry, final Map<String, MicroserviceDto> microservicesFromPersistence) {
+        final Map<String, MicroserviceDto> mergedDtoList = new HashMap<>();
+        mergedDtoList.putAll(extractRegisteredButNotPersisted(microservicesFromRegistry, microservicesFromPersistence));
+        mergedDtoList.putAll(extractPersistedButNotRegistered(microservicesFromRegistry, microservicesFromPersistence));
+        mergedDtoList.putAll(mergeRegisteredAndPersisted(microservicesFromRegistry, microservicesFromPersistence));
+        return mergedDtoList;
+    }
 
-        return result;
+    private Map<String, MicroserviceDto> mergeRegisteredAndPersisted(Map<String, MicroserviceDto> microservicesFromRegistry, Map<String, MicroserviceDto> microservicesFromPersistence) {
+        return microservicesFromRegistry.values()
+                .stream()
+                .filter(dto -> microservicesFromPersistence.containsKey(dto.getId()))
+                .map(dto -> mergeMicroservice(dto, microservicesFromPersistence.get(dto.getId()))).collect(Collectors.toMap(MicroserviceDto::getId, Function.identity()));
+    }
+
+    private Map<String, MicroserviceDto> extractPersistedButNotRegistered(Map<String, MicroserviceDto> microservicesFromRegistry, Map<String, MicroserviceDto> microservicesFromPersistence) {
+        return microservicesFromPersistence.values()
+                .stream()
+                .filter(dto -> !microservicesFromRegistry.containsKey(dto.getId()))
+                .collect(Collectors.toMap(MicroserviceDto::getId, Function.identity()));
+    }
+
+    private Map<String, MicroserviceDto> extractRegisteredButNotPersisted(Map<String, MicroserviceDto> microservicesFromRegistry, Map<String, MicroserviceDto> microservicesFromPersistence) {
+        return microservicesFromRegistry.values()
+                .stream()
+                .filter(dto -> !microservicesFromPersistence.containsKey(dto.getId()))
+                .collect(Collectors.toMap(MicroserviceDto::getId, Function.identity()));
     }
 
     public MicroserviceDto mergeMicroservice(MicroserviceDto sourceService, MicroserviceDto serviceToMerge) {
         Assert.notNull(sourceService);
 
-        if (serviceToMerge == null){
+        if (serviceToMerge == null) {
             return sourceService;
         }
 
         sourceService.setId(serviceToMerge.getId());
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getLabel())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getLabel())) {
             sourceService.setLabel(serviceToMerge.getLabel());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getDescription())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getDescription())) {
             sourceService.setDescription(serviceToMerge.getDescription());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getBitbucketUrl())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getBitbucketUrl())) {
             sourceService.setBitbucketUrl(serviceToMerge.getBitbucketUrl());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getIgnoredCommitters())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getIgnoredCommitters())) {
             sourceService.setIgnoredCommitters(serviceToMerge.getIgnoredCommitters());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getFdOwner())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getFdOwner())) {
             sourceService.setFdOwner(serviceToMerge.getFdOwner());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getTags())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getTags())) {
             sourceService.setTags(serviceToMerge.getTags());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getMicroserviceUrl())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getMicroserviceUrl())) {
             sourceService.setMicroserviceUrl(serviceToMerge.getMicroserviceUrl());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getIpAddress())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getIpAddress())) {
             sourceService.setIpAddress(serviceToMerge.getIpAddress());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getNetworkZone())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getNetworkZone())) {
             sourceService.setNetworkZone(serviceToMerge.getNetworkZone());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getDocumentationLink())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getDocumentationLink())) {
             sourceService.setDocumentationLink(serviceToMerge.getDocumentationLink());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getBuildMonitorLink())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getBuildMonitorLink())) {
             sourceService.setBuildMonitorLink(serviceToMerge.getBuildMonitorLink());
         }
 
-        if (StringUtils.isNotEmpty(serviceToMerge.getMonitoringLink())){
+        if (StringUtils.isNotEmpty(serviceToMerge.getMonitoringLink())) {
             sourceService.setMonitoringLink(serviceToMerge.getMonitoringLink());
         }
         if (serviceToMerge.isExternal()) {
@@ -112,7 +127,7 @@ public class MicroserviceMergeService {
 
 
     private void mergeHosts(MicroserviceDto sourceService, MicroserviceDto serviceToMerge) {
-        if (serviceToMerge.getHosts() != null && !serviceToMerge.getHosts().isEmpty()){
+        if (serviceToMerge.getHosts() != null && !serviceToMerge.getHosts().isEmpty()) {
             sourceService.setHosts(serviceToMerge.getHosts());
         }
     }
